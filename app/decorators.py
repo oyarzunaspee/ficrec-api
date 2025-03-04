a113b9cb0ed919aafcce6a9893525bff9f0e6c7e
oyarzunaspee <oyarzunaspee.work@gmail.com>
2025-03-04 14:07:25 -0300

    fix imports

---
+++ app/decorators.py
@@ -0,0 +1,38 @@
+from functools import wraps
+from models import User, Token
+
+def public_user(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        view = args[0]
+        view.public = True
+        view.base = "public"
+
+        username = kwargs["username"]
+
+        await view.redis.open()
+        redis_key = f"public_user_{username}"
+        redis_query = await view.redis.get_redis(redis_key)
+        if redis_query:
+            view.user_id = redis_query
+        
+        if not redis_query:
+            user = await User.find_by_username(username)
+            view.user_id = str(user.id)
+            await view.redis.set_redis(redis_key, str(user.id))
+
+        return await func(*args, **kwargs)
+    return wrapper
+
+def auth_user(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        view = args[0]
+        view.public = False
+        view.base = "auth"
+
+        token = kwargs["token"]
+        view.user_id = Token(access_token=token).get_current_user()
+
+        return await func(*args, **kwargs)
+    return wrapper
